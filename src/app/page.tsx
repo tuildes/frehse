27680efacb6315b6f98faed038b35c5c449f03d0
tuildes <frehsegustavo@gmail.com>
2025-02-09 14:56:42 -0300

    feat: introducao finalizada, falta mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import styles from "./page.module.css";
 
+import Introduction from "@/components/Introduction/introduction";
+
 export default function Home() {
   return (
-    <div className={styles.page}></div>
+    <div className={styles.page}>
+        <Introduction />
+    </div>
   );
 }
---
+++ src/components/Introduction/introduction.tsx
@@ -0,0 +1,62 @@
+import Styles from "./introduction.module.css"
+import Image from "next/image"
+
+import { IconBrandGithub, IconBrandLinkedin } from '@tabler/icons-react';
+
+const linkedinLink:string = "https://linkedin.com/in/gfrehse"
+const githubLink:string = "https://github.com/tuildes"
+
+const sizeIcon:number = 48
+
+export default function Introduction() {
+    return(
+        <div className={Styles.introduction__wrapper}>
+            <div className={Styles.introduction__content}>
+                <div>
+                    <Image 
+                        className={Styles.introduction__profilePhoto}
+                        src="/home/gustavo.png"
+                        alt="Gustavo Frehse"
+                        width={500}
+                        height={500}
+                    />
+                </div>
+                <div className={Styles.introduction__about}>
+                    <div>
+                        <h1>Who am I?</h1>
+                        <span>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu volutpat nibh. 
+                            Maecenas facilisis neque condimentum vestibulum imperdiet. Nulla  tempus pretium 
+                            massa commodo blandit. <strong>Fusce luctus eros</strong> quis ligula  consequat blandit.
+                        </span>
+                        <div className={Styles.introduction__links}>
+                            <a href={githubLink} target="__blank">
+                                <IconBrandGithub className={Styles.icon} color="white" size={sizeIcon} />
+                            </a>
+                            <a href={linkedinLink} target="__blank">
+                                <IconBrandLinkedin className={Styles.icon} color="white" size={sizeIcon} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className={Styles.introduction__name}>
+                    <Image 
+                        className={Styles.introduction__nameBackground}
+                        src="/home/florest-thiago-falcao.jpg"
+                        alt="Florest"
+                        width={600}
+                        height={1000}
+                    />
+                    <div className={Styles.introduction__nameText}>
+                        <span>
+                            Gustavo 
+                            <br/>
+                            Frehse
+                        </span>
+                        <small>tuildes</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
