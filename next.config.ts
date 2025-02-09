d22f8f630e0f2ff282b53e5756fbfc08b4f3e388
tuildes <frehsegustavo@gmail.com>
2025-02-09 18:21:57 -0300

    feat: deploy settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+    reactStrictMode: true,
+    basePath: '/personal-site',
 };
 
 export default nextConfig;
