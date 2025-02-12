efd1020b33ce361510c7db6879f77fe2b48dfa5a
tuildes <frehsegustavo@gmail.com>
2025-02-12 01:09:35 -0300

    feat: ocnfiguracoes de deploy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: "export",
+    basePath: "/pessoal-site",
+    images: {
+        unoptimized: true,
+    },
+
 };
 
 export default nextConfig;
