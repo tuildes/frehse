4e69af4742bbefae080fb24236ccd2f2175a9c1d
tuildes <frehsegustavo@gmail.com>
2025-02-12 13:57:38 -0300

    feat: responsividade mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Lexend } from 'next/font/google'
 import './globals.css'
 
 const lexend = Lexend({ subsets: ['latin'] })
 
-// Components
+// Componentes
 import Navbar from '@/components/Navbar/navbar'
 import Footer from '@/components/Footer/footer'
+
+export const viewport: Viewport = {
+    themeColor: "#000000",
+    initialScale: 1,
+    width: "device-width",
+    maximumScale: 1,
+    viewportFit: "cover",
+};
 
 export const metadata: Metadata = {
     title: 'Gustavo Frehse',
